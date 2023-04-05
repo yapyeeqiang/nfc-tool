1c705dddf8034671fd93c43dc06c70f6943ba38f
yapyeeqiang <yapyeeqiang@gmail.com>
2023-04-05 15:32:41 +0800

    init: setup

---
+++ app.js
@@ -0,0 +1,23 @@
+// check if NFC is supported by the browser
+if ('NDEFReader' in window) {
+    const reader = new NDEFReader()
+
+    // handle NFC reading
+    reader.addEventListener('reading', (event) => {
+        const tagMessage = event.message.records[0].data
+        const tagMessageString = new TextDecoder().decode(tagMessage)
+        document.querySelector('.tag-message').innerHTML = `<p>${tagMessageString}</p>`
+        document.querySelector('.tag-message').style.display = 'block'
+        document.querySelector('#scan-prompt').style.display = 'none'
+    })
+
+    // handle NFC errors
+    reader.addEventListener('error', (event) => {
+        document.querySelector('#nfc-error').innerHTML = `Error: ${event.message}`
+    })
+
+    // start listening for NFC scans
+    reader.scan()
+} else {
+    document.querySelector('#nfc-error').innerHTML = 'NFC is not supported by this device/browser.'
+}
